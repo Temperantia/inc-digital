fa2b0cee76f4f68fe5e1b57685ab781682e39026
Florent Bouisset <florent.bouisset@gmail.com>
2020-08-16 19:29:52 +0200

    add contact form feature

---
+++ layouts/contactForm/budget.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-card flat color="transparent">
+    <v-row>
+      <v-col align="start" justify="start">
+        <div class="stepTitle">What's your budget?</div>
+        <div class="stepDescription">
+          A transparent budget will help us ensure expectations are met.
+          Not sure? Ballparks are fine.
+        </div>
+      </v-col>
+    </v-row>
+    <v-card-text>
+      <v-row>
+        <template>
+          <v-container class="pa-4">
+            <div
+              :class="disabled ? 'grey--text':'black--text'"
+              style="font-size:20px; font-weight:500"
+            >${{range[0]/1000}}K — ${{range[1]/1000}}K</div>
+          </v-container>
+        </template>
+
+        <v-col class="px-4">
+          <v-range-slider
+            id="budgetSlider"
+            v-model="range"
+            :max="max"
+            :min="min"
+            :disabled="disabled"
+            step="1000"
+            hide-details
+            track-color="grey"
+            class="align-center"
+          >
+            <template v-slot:prepend>
+              <div style="font-size:20px">${{min/1000}}K</div>
+            </template>
+            <template v-slot:append>
+              <div style="font-size:20px">${{max/1000}}K</div>
+            </template>
+          </v-range-slider>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col align="start" class="ma-6">
+          <v-btn-toggle>
+            <v-btn @click="disabled = !disabled">I don't know my budget</v-btn>
+          </v-btn-toggle>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      min: 5000,
+      max: 45000,
+      range: [15000, 30000],
+      disabled: false,
+    };
+  },
+  methods: {
+    form() {
+      if (this.disabled) {
+        return {
+          budget: "budget unknown",
+        };
+      } else
+        return {
+          budgetmin: this.range[0],
+          budgetmax: this.range[1],
+        };
+    },
+  },
+};
+</script>
+
+<style>
+.stepTitle {
+  font-size: 1.8em;
+  font-weight: 500;
+  margin-bottom: 20px;
+}
+
+.stepDescription {
+  font-size: 1.1em;
+}
+</style>
---
+++ layouts/contactForm/multiStepForm.vue
@@ -0,0 +1,159 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-stepper v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1" color="orange">Contact</v-stepper-step>
+        <v-stepper-step :complete="e1 > 2" step="2" color="orange">Technology</v-stepper-step>
+        <v-stepper-step :complete="e1 > 3" step="3" color="orange">Budget</v-stepper-step>
+        <v-stepper-step step="4" color="orange">Lastly</v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items style="height:550px">
+        <v-stepper-content class="stepperContent" step="1">
+          <v-container style="height:450px">
+            <contactPage ref="contact"></contactPage>
+          </v-container>
+          <v-btn class="white--text" color="orange" @click="nextPage()">Continue</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content class="stepperContent" step="2">
+          <v-container style="height:450px">
+            <technology ref="technology"></technology>
+          </v-container>
+
+          <v-btn class="white--text" color="orange" @click="nextPage()">Continue</v-btn>
+          <v-btn text @click="e1 -= 1">Cancel</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content class="stepperContent" step="3">
+          <v-container style="height:450px">
+            <budget ref="budget"></budget>
+          </v-container>
+
+          <v-btn color="orange" class="white--text" @click="nextPage()">Continue</v-btn>
+          <v-btn text @click="e1 -= 1">Cancel</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content class="stepperContent" step="4">
+          <v-container style="height:450px">
+            <finalStep ref="finalStep"></finalStep>
+          </v-container>
+
+          <v-btn color="orange" class="white--text" @click="submit()">Submit</v-btn>
+          <v-btn text @click="e1 -= 1">Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+
+      <v-overlay :absolute="absolute" :value="overlay">
+        <v-col align="center" justify="center" v-if="sending">
+          <v-progress-circular indeterminate size="64"></v-progress-circular>
+          <v-container>
+            <p>Sending</p>
+          </v-container>
+        </v-col>
+
+        <v-col align="center" justify="center" v-else>
+          <v-icon dark right size="64">mdi-checkbox-marked-circle</v-icon>
+          <v-container>
+            <p>Sent</p>
+          </v-container>
+        </v-col>
+      </v-overlay>
+    </v-stepper>
+  </v-row>
+</template>
+
+<script>
+import ContactPage from "~/layouts/contactForm/contact.vue";
+import Technology from "~/layouts/contactForm/technology.vue";
+import Budget from "~/layouts/contactForm/budget.vue";
+import FinalStep from "~/layouts/contactForm/finalStep.vue";
+import Email from "~/plugins/mailService.js";
+
+export default {
+  data() {
+    return {
+      absolute: true,
+      overlay: false,
+      sending: false,
+      e1: 1,
+    };
+  },
+  methods: {
+    nextPage: function () {
+      const maxPage = 3;
+      switch (this.e1) {
+        case 1:
+          if (this.$refs.contact.validate()) {
+            this.e1++;
+          }
+          break;
+
+        case 2:
+          if (this.$refs.technology.validate()) {
+            this.e1++;
+          }
+          break;
+
+        case 3:
+          this.e1++;
+          break;
+
+        default:
+          break;
+      }
+    },
+    submit: async function () {
+      if (this.$refs.finalStep.validate()) {
+        this.sending = true;
+        this.overlay = true;
+        await this.sendDataWithMail();
+        this.sending = false;
+      }
+    },
+    back: function () {
+      this.sending = false;
+      this.overlay = false;
+    },
+
+    getFormInfo: async function () {
+      let data = [];
+      data.push(await this.$refs.contact.form());
+      data.push(await this.$refs.technology.form());
+      data.push(await this.$refs.budget.form());
+      data.push(await this.$refs.finalStep.form());
+      return data;
+    },
+
+    createHTMLforMail: async function () {
+      const objArray = await this.getFormInfo();
+      let output = "<h2>Un nouveau prospect a renseigné ses informations</h2>";
+      for (let objIndex in objArray) {
+        let obj = objArray[objIndex];
+        for (let property in obj) {
+          output += `<p><strong>${property}</strong> : ${obj[property]} </p>`;
+        }
+      }
+      return output;
+    },
+
+    sendDataWithMail: async function () {
+      const mailList = ["inclusive.corporation@gmail.com"];
+      const mailContent = await this.createHTMLforMail();
+      const resp = await Email.send({
+        SecureToken: "e82ccc8d-7bfd-4d7b-b25f-7be55b7bf343",
+        To: mailList,
+        From: "inclusive.corporation@gmail.com",
+        Subject: "Test email",
+        Body: mailContent,
+      });
+    },
+  },
+  components: {
+    contactPage: ContactPage,
+    technology: Technology,
+    budget: Budget,
+    finalStep: FinalStep,
+  },
+};
+</script>
