c577b621f5214bf0b3aba54b360bb920aa70e5cc
Florent Bouisset <florent.bouisset@gmail.com>
2020-09-05 18:05:37 +0200

    refacto header

--- components/header.vue
+++ components/header.vue
@@ -12,14 +12,22 @@
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
-    <a
-      style="margin-right: 150px; color: #ff9800; text-decoration: none;"
-      href="#home"
-    >{{$t('navbar.home')}}</a>
-    <a
-      style="margin-right: 150px; color: #adecf4; text-decoration: none;"
-      href="#expertise"
-    >{{$t('navbar.expertise')}}</a>
+    <NuxtLink
+      v-for="(item, i) in items"
+      :key="i"
+      :to="item.route"
+      class="text-no-wrap my-2 mx-6 white--text"
+      style="text-decoration: none;"
+    >{{$t(item.text)}}</NuxtLink>
+
+    <Nuxt-link
+      class="text-no-wrap py-1 px-6 accent--text"
+      style="text-decoration: none;
+            border-width: 1px;
+            border-style: solid;
+            "
+      to="/"
+    >{{$t('navbar.contact')}}</Nuxt-link>
 
     <NuxtLink
       v-if="$i18n.locale === 'en'"
@@ -36,10 +44,30 @@
       active-class="none"
       exact
     >{{ $t('links.english') }}</NuxtLink>
-    <a style="margin-right: 100px;"></a>
   </v-toolbar>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    items: [
+      {
+        route: "#home",
+        text: "navbar.home",
+      },
+      {
+        route: "#expertise",
+        text: "navbar.expertise",
+      },
+      {
+        route: "#home", //TO DO : update route
+        text: "navbar.work",
+      },
+      {
+        route: "#home", //TO DO : update route
+        text: "navbar.team",
+      },
+    ],
+  }),
+};
 </script>
