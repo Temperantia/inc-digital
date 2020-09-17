acf2f90db3151c2557e7b769d65d496ba2d717d8
Florent Bouisset <florent.bouisset@gmail.com>
2020-09-17 02:04:16 +0200

    add responsive for mobile resolution

--- components/footer.vue
+++ components/footer.vue
@@ -2,21 +2,27 @@
   <v-footer>
     <v-container fluid style="width:70%">
       <v-row>
-        <div class="grey--text ma-4" style="font-size:18px">{{$t('footer.clientSupport')}}</div>
-        <div class="grey--text ma-4" style="font-size:18px">{{$t('footer.termsAndPrivacy')}}</div>
-        <v-spacer></v-spacer>
-        <v-icon
-          class="ma-2"
-          v-for="(item, i) in socialMediaItems"
-          :key="i"
-          @click="goto(item.url)"
-        >{{item.iconID}}</v-icon>
+        <v-col cols="6" md="3">
+          <div class="grey--text ma-4" style="font-size:18px">{{$t('footer.clientSupport')}}</div>
+        </v-col>
+        <v-col cols="6" md="3">
+          <div class="grey--text ma-4" style="font-size:18px">{{$t('footer.termsAndPrivacy')}}</div>
+        </v-col>
+        <v-col cols="0" md="3"></v-col>
+        <v-col cols="12" md="3" class="d-flex">
+          <v-icon
+            class="ma-2"
+            v-for="(item, i) in socialMediaItems"
+            :key="i"
+            @click="goto(item.url)"
+          >{{item.iconID}}</v-icon>
+        </v-col>
       </v-row>
 
       <v-divider class="my-4" color="black"></v-divider>
 
       <v-row>
-        <v-col cols="8" align-self="center">
+        <v-col cols="12" md="8" align-self="center">
           <span
             class="grey--text"
           >&copy; {{ new Date().getFullYear() }} {{$t('footer.copyright')}} {{$t('footer.useOfThisSite')}}</span>
@@ -26,7 +32,7 @@
             href="https://en.wikipedia.org/wiki/Terms_of_service"
           >{{$t('footer.termsOfUse')}}</a>
         </v-col>
-        <v-col cols="4" style="display: flex; align-items: center;">
+        <v-col cols="12" md="4" style="display: flex; align-items: center;">
           <span class="grey--text">{{$t('footer.descriptionPrefix')}}</span>
           <img class="mx-2" width="100" src="/inclusiveLogotype.png" />
           <span class="grey--text">{{$t('footer.descriptionSuffix')}}</span>
